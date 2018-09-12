c31bef1fa024e0ce310e282b202cc31e840d9b14
Diya Burman <1991diya.burman@gmail.com>
2018-09-12 16:11:00 -0400

    pushing face detection API code

---
+++ server.js
@@ -0,0 +1,39 @@
+//plan for the API structure in the APIstructurePlan.txt
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'diya91',
+    password : '',
+    database : 'face-detection'
+  }
+});
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=> { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt)) // if we remember from the 'Advanced Functions' lecture, we do not need to send req and res as well becasue we're first running this function with db and bcrypt. And then it automatically receives req and res.
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
